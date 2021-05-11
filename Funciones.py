75e30578df2b35c300ef9ba5e38a488ab93f8780
Cynthiag3 <83793975+Cynthiag3@users.noreply.github.com>
2021-05-10 19:17:52 -0600

    Create Funciones.py
    Programa funciones v1

---
+++ Funciones.py
@@ -0,0 +1,42 @@
+def areaCuadrado():
+  lado=int(input("Capture el lado: "))
+  area=lado*lado
+  print("El area es = "+str(area))
+  perimetro=lado*4
+  print("El perimetro es = "+str(perimetro))
+
+def areaTriangulo():
+  base=int(input("Ingresa la base"))
+  altura=int(input("Ingresa la altura"))
+  area=(base*altura)/2
+  perimetro=base*3
+  print("El area del triangulo es = "+str(area))
+  print("El area del perimetro es = "+str(perimetro))
+
+
+print("Calcula areas y perimetros")
+print("Opciones: ")
+print("1. Cuadrado")
+print("2. Triangulo")
+print("3. Circulo")
+
+"""
+figura1= "1. cuadrado"
+print(figura1)
+
+figura2="2. triangulo"
+print(figura2)
+"""
+
+numero=int(input("Ingresa el numero de la opcion a elegir"))
+
+if numero==1:
+  areaCuadrado()
+elif numero==2:
+  areaTriangulo()  
+
+
+
+
+
+ 
